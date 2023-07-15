c83f124adbe4f62fc7dbd541645f2ad4df7dd399
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-07-14 22:10:22 -0400

    added readme

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -22,27 +22,28 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const { title, text } = req.body
   if (req.body) {
-    const newNote = { title, text, note_id:uuidv4() }
-    readAndAppend(newNote, './db/db.json', (err,data) => {
-        if (err) {
+    const newNote = { title, text, note_id: uuidv4() }
+    readAndAppend(newNote, './db/db.json', (err, data) => {
+      if (err) {
         console.log(err);
         res.status(500).send("Error reading database")
-    } else { 
+      } else {
         const notes = JSON.parse(data);
         notes.push(newNote);
         writeToFile("./db/db.json", JSON.stringify(notes), (err) => {
-            if (err) {
-              console.log(err);
-              res.status(500).send("Error writing to database");
-            } else {
-              res.json(newNote);
-            }
-          });
-        }
-      });
-    } else {
-      res.status(400).send('Bad Request');
-    }
-  });
+          if (err) {
+            console.log(err);
+            res.status(500).send("Error writing to database");
+          } else {
+            res.json(newNote);
+          }
+        });
+      }
+    });
+  } else {
+    res.status(400).send('Bad Request');
+  }
+});
 
-    module.exports = router;
+//export
+module.exports = router;
